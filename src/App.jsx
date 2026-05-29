898078b2adb87b76370ad1b4143202664f269268
mappy0922 <mr.masa0329@gmail.com>
2026-05-30 01:00:16 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import * as d3 from "d3";
+import { useState } from "react";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="top">
+      <h1>地震可視化マップ</h1>
     </div>
   );
 }
